0b6d4f481f835904724b3108eb3cca5d42151b0d
“MemoOrtizgit config --global user.email “memodor2001@gmail.com <memodor2001@gmail.com>
2024-02-24 14:51:30 -0600

    Se añadio cambios a la salida Formateada como es el tipo de Letra y que reciba de parametro un titulo, lo cual lo hace dinamico

--- misUtilerias/SalidaFormateada.java
+++ misUtilerias/SalidaFormateada.java
@@ -1,14 +1,17 @@
 package misUtilerias;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class SalidaFormateada {
-    public static void imprimeConScroll(String cadena) {
+    public static void imprimeConScroll(String cadena,String titulo) {
 
-        JTextArea area = new JTextArea(cadena, 35, 30); //los 20 20 son las dimensiones de la ventwnw
+        JTextArea area = new JTextArea(cadena, 35, 30); //los 20 20 son las dimensiones de la ventana
+        Font font = new Font("Arial", Font.PLAIN, 14); // Por ejemplo, Arial 14pt
+        area.setFont(font);
         JScrollPane panel = new JScrollPane(area,
                 JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED,
                 JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
-        JOptionPane.showMessageDialog(null, panel);
+        JOptionPane.showMessageDialog(null, panel,titulo,JOptionPane.INFORMATION_MESSAGE);
     }
 }
