9191133edd6f8bbf971bab7043826258bc5c53ed
“MemoOrtizgit config --global user.email “memodor2001@gmail.com <memodor2001@gmail.com>
2024-02-25 02:37:17 -0600

    Se añadio a la opcion1 .clear() para limpiar los arraylist, se modularizo el menu y el main se simplifico mandando a llamar dos metodos

--- u2/Practica2.java
+++ u2/Practica2.java
@@ -30,16 +30,15 @@ public class Practica2 {
             FileReader fr = new FileReader(nombreArchivo);
             BufferedReader br = new BufferedReader(fr);
             String linea;
-            //leer el archivo por lineas
-            while((linea = br.readLine())!=null) {
-                //Dividir la línea en palabras separadas por comas o espacios
-                String[] palabrasLinea = linea.split("[,\\s]+");
-                //Agregar las palabras al ArrayList
-                palabrasDeArchivo.addAll(Arrays.asList(palabrasLinea));
-                //cerrar lectura de archivo
-                br.close();
-                JOptionPane.showMessageDialog(null, "Archivo leido con exito");
+
+            while((linea = br.readLine())!=null) {  //leer el archivo por lineas
+                String[] palabrasLinea = linea.split("[,\\s]+");    //Dividir la línea en palabras separadas por comas o espacios
+                palabrasDeArchivo.addAll(Arrays.asList(palabrasLinea)); //Agregar las palabras al ArrayList
             }
+
+            br.close();//cerrar lectura de archivo
+            JOptionPane.showMessageDialog(null, "Archivo leido con exito");
+            return true;
 
         }catch (FileNotFoundException e) {
             JOptionPane.showMessageDialog(null, "No se pudo abrir el archivo");
@@ -48,12 +47,7 @@ public class Practica2 {
         }
         return false;
     }
-    public static void main (String[]args){
-        boolean archivoLeido = leerArchivo();
-        if (!archivoLeido) {
-            JOptionPane.showMessageDialog(null, "El archivo no pudo ser leído. Se cerrará la ventana.");
-            return; // Salir del programa
-        }
+    private static void realizarMenu(){
         String menu = """
                 1. Comparar Archivo con Lenguaje 1
                 2. Comparar Archivo con Lenguaje 2
@@ -63,95 +57,108 @@ public class Practica2 {
         int opcion = 0;
         while(opcion != 4){
             try{
-            opcion = Integer.parseInt(JOptionPane.showInputDialog(null,menu));
+                opcion = Integer.parseInt(JOptionPane.showInputDialog(null,menu));
                 if (opcion < 1 || opcion > 4) {
                     JOptionPane.showMessageDialog(null, "Por favor, ingrese una opción válida.");
                     continue;
                 }
-            switch (opcion){
+                switch (opcion){
 
-                case 1:
-                    String titulo1 = "COINCIDENCIA EXITOSA";
-                    String titulo2 = "COINCIDENCIA NO VALIDA";
-                    String expresionRegular = "(hola)+(0|1)*";
-                    Pattern pattern = Pattern.compile(expresionRegular);
+                    case 1:
+                        // Limpiar las listas antes de comenzar la comparación
+                        palabrasValidadas.clear();
+                        palabrasNoValidas.clear();
 
-                    // se usa un foreach para evaluar el arraylist en cada elemento, el cual contiene elementos palabras
-                    for (String palabra:palabrasDeArchivo) {
-                        boolean coincidencia = pattern.matcher(palabra).matches();
+                        String titulo1 = "COINCIDENCIA EXITOSA";
+                        String titulo2 = "COINCIDENCIA NO VALIDA";
+                        String expresionRegular = "(hola)+(0|1)*";
+                        Pattern pattern = Pattern.compile(expresionRegular);
+
+                        // se usa un foreach para evaluar el arraylist en cada elemento, el cual contiene elementos palabras
+                        for (String palabra:palabrasDeArchivo) {
+                            boolean coincidencia = pattern.matcher(palabra).matches();
                             if(coincidencia){
                                 palabrasValidadas.add(palabra);
                             }else{
                                 palabrasNoValidas.add(palabra);
                             }
-                        //System.out.println("Palabra: " + palabra + " - Coincide con : " + coincidencia);
-                    }
-
-                    String palabrasValidadasString = String.join("\n",palabrasValidadas);
-                    String palabrasNoValidasString = String.join("\n",palabrasNoValidas);
-
-                    SalidaFormateada.imprimeConScroll("Las palabras que SI coinciden con la Expresion Regular "+expresionRegular+"\n"+"\n"+palabrasValidadasString,titulo1);
-                    SalidaFormateada.imprimeConScroll("Las palabras que NO coinciden con la Expresion Regular "+expresionRegular+"\n"+"\n"+palabrasNoValidasString,titulo2);
-
-                    break;
-                case 2:
-                    // Limpiar las listas antes de comenzar la comparación
-                    palabrasValidadas.clear();
-                    palabrasNoValidas.clear();
-
-                    String titulo1Caso3 = "COINCIDENCIA EXITOSA - Lenguaje 2";
-                    String titulo2Caso3 = "COINCIDENCIA NO VALIDA - Lenguaje 2";
-                    String expresionRegular2 = "(aa|bb)#+(123)*";
-
-                    // Iterar sobre las palabras del archivo
-                    for (String palabra : palabrasDeArchivo) {
-                        int index = 0;
-                        int length = palabra.length();
-
-                        // Verificar si la cadena cumple con el patrón
-                        if (length < 2 || (palabra.charAt(0) != 'a' && palabra.charAt(0) != 'b') || palabra.charAt(0) != palabra.charAt(1)) {
-                            palabrasNoValidas.add(palabra);
-                            continue; // Saltar a la siguiente palabra
+                            //System.out.println("Palabra: " + palabra + " - Coincide con : " + coincidencia);
                         }
 
-                        index += 2; // Avanzar el índice si la cadena comienza con "aa" o "bb"
+                        String palabrasValidadasString = String.join("\n",palabrasValidadas);
+                        String palabrasNoValidasString = String.join("\n",palabrasNoValidas);
 
-                        // Verificar la presencia de "#" después de "aa" o "bb"
-                        while (index < length && palabra.charAt(index) == '#') {
-                            index++; // Avanzar el índice si encuentra "#"
+                        SalidaFormateada.imprimeConScroll("Las palabras que SI coinciden con la Expresion Regular "+expresionRegular+"\n"+"\n"+palabrasValidadasString,titulo1);
+                        SalidaFormateada.imprimeConScroll("Las palabras que NO coinciden con la Expresion Regular "+expresionRegular+"\n"+"\n"+palabrasNoValidasString,titulo2);
+
+                        break;
+                    case 2:
+                        // Limpiar las listas antes de comenzar la comparación
+                        palabrasValidadas.clear();
+                        palabrasNoValidas.clear();
+
+                        String titulo1Caso3 = "COINCIDENCIA EXITOSA - Lenguaje 2";
+                        String titulo2Caso3 = "COINCIDENCIA NO VALIDA - Lenguaje 2";
+                        String expresionRegular2 = "(aa|bb)#+(123)*";
+
+                        // Iterar sobre las palabras del archivo
+                        for (String palabra : palabrasDeArchivo) {
+                            int index = 0;
+                            int length = palabra.length();
+
+                            // Verificar si la cadena cumple con el patrón
+                            if (length < 2 || (palabra.charAt(0) != 'a' && palabra.charAt(0) != 'b') || palabra.charAt(0) != palabra.charAt(1)) {
+                                palabrasNoValidas.add(palabra);
+                                continue; // Saltar a la siguiente palabra
+                            }
+
+                            index += 2; // Avanzar el índice si la cadena comienza con "aa" o "bb"
+
+                            // Verificar la presencia de "#" después de "aa" o "bb"
+                            while (index < length && palabra.charAt(index) == '#') {
+                                index++; // Avanzar el índice si encuentra "#"
+                            }
+
+                            // Verificar la presencia de "123" después de los "#"
+                            while (index + 3 <= length && palabra.charAt(index) == '1' && palabra.charAt(index + 1) == '2' && palabra.charAt(index + 2) == '3') {
+                                index += 3; // Avanzar el índice si encuentra "123"
+                            }
+
+                            // Si el índice alcanza la longitud de la palabra, es válida
+                            if (index == length) {
+                                palabrasValidadas.add(palabra);
+                            } else {
+                                palabrasNoValidas.add(palabra);
+                            }
                         }
 
-                        // Verificar la presencia de "123" después de los "#"
-                        while (index + 3 <= length && palabra.charAt(index) == '1' && palabra.charAt(index + 1) == '2' && palabra.charAt(index + 2) == '3') {
-                            index += 3; // Avanzar el índice si encuentra "123"
-                        }
+                        // Convertir las listas a cadenas
+                        String palabrasValidadasStringCaso3 = String.join("\n", palabrasValidadas);
+                        String palabrasNoValidasStringCaso3 = String.join("\n", palabrasNoValidas);
 
-                        // Si el índice alcanza la longitud de la palabra, es válida
-                        if (index == length) {
-                            palabrasValidadas.add(palabra);
-                        } else {
-                            palabrasNoValidas.add(palabra);
-                        }
-                    }
+                        // Mostrar los resultados
+                        SalidaFormateada.imprimeConScroll("Cadenas que SI coinciden con la ER "+expresionRegular2+"\n"+"\n"+palabrasValidadasStringCaso3,titulo1Caso3);
+                        SalidaFormateada.imprimeConScroll("Cadenas que NO coinciden con la ER"+expresionRegular2+"\n"+"\n"+palabrasNoValidasStringCaso3, titulo2Caso3);
 
-                    // Convertir las listas a cadenas
-                    String palabrasValidadasStringCaso3 = String.join("\n", palabrasValidadas);
-                    String palabrasNoValidasStringCaso3 = String.join("\n", palabrasNoValidas);
-
-                    // Mostrar los resultados
-                    SalidaFormateada.imprimeConScroll("Cadenas que SI coinciden con la ER "+expresionRegular2+"\n"+"\n"+palabrasValidadasStringCaso3,titulo1Caso3);
-                    SalidaFormateada.imprimeConScroll("Cadenas que NO coinciden con la ER"+expresionRegular2+"\n"+"\n"+palabrasNoValidasStringCaso3, titulo2Caso3);
-
-                    break;
-                case 3:
-                    break;
-                case 4:
-                    break;
-            }
-        }catch (NumberFormatException e) {
+                        break;
+                    case 3:
+                        break;
+                    case 4:
+                        JOptionPane.showMessageDialog(null, "Hasta luego");
+                        break;
+                        
+                }
+            }catch (NumberFormatException e) {
                 JOptionPane.showMessageDialog(null, "Ingrese un número válido del menu.");
             }
         }
     }
-    
+    public static void main (String[]args){
+        boolean archivoLeido = leerArchivo();
+        if (!archivoLeido) {
+            JOptionPane.showMessageDialog(null, "El archivo no pudo ser leído. Se cerrará la ventana.","Error",JOptionPane.ERROR_MESSAGE);
+            return; // Salir del programa
+        }
+        realizarMenu();
+    }
 }
