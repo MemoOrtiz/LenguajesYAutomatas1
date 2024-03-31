6827129ec8a655200ea52bfc65ce039bfde7c0f9
“MemoOrtizgit config --global user.email “memodor2001@gmail.com <memodor2001@gmail.com>
2024-03-31 11:53:44 -0600

    Metodo tiene contenido archivo y metodo lectura de Archivo

--- u4/Practica4.java
+++ u4/Practica4.java
@@ -1,5 +1,57 @@
 package u4;
 
+import javax.swing.*;
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Practica4 {
+    static String nombreArchivo = "./u4/practica4.txt";
+    static List<String> palabrasDeArchivo = new ArrayList<>();
+    private static boolean archivoLecturaTieneContenido() {
+        try {
+            FileReader fr = new FileReader(nombreArchivo);
+            BufferedReader br = new BufferedReader(fr);
+            boolean tieneContenido = br.readLine() != null; // Verificar si hay al menos una línea para leer
+            br.close();
+            return tieneContenido; //retorno "true"
+        } catch (IOException e) {
+            JOptionPane.showMessageDialog(null, "Conflicto para leer el archivo");
+            return false;
+        }
+    }
+
+    private static boolean leerArchivo() {
+        if (!archivoLecturaTieneContenido()) {
+            JOptionPane.showMessageDialog(null, "El archivo está vacío");
+            return false;
+        }
+        try {
+            FileReader fr = new FileReader(nombreArchivo);
+            BufferedReader br = new BufferedReader(fr);
+            String linea;
+
+
+            String[] palabrasLinea = new String[0];
+            while ((linea = br.readLine()) != null) {  //leer el archivo por lineas
+                palabrasLinea = linea.split("[,]+");
+                palabrasDeArchivo.addAll(Arrays.asList(palabrasLinea)); //Agregar las palabras al ArrayList
+            }
+
+            br.close();//cerrar lectura de archivo
+            JOptionPane.showMessageDialog(null, "Archivo leido con exito");
+            return true;
+
+        } catch (FileNotFoundException e) {
+            JOptionPane.showMessageDialog(null, "No se pudo abrir el archivo, porque no existe o no se encuentra");
+        } catch (IOException e) {
+            JOptionPane.showMessageDialog(null, "Conflicto para leer el archivo");
+        }
+        return false;
+    }
 
 }
