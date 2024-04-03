2e75f610497d28009eb59d2e5e46a9224279bd38
“MemoOrtizgit config --global user.email “memodor2001@gmail.com <memodor2001@gmail.com>
2024-04-02 23:14:30 -0600

    Se utiliza las clases PalabraPosicion y SinglyLinkList para reemplazar al arrayList palabrasDeArchivo

--- u4/Practica4.java
+++ u4/Practica4.java
@@ -14,6 +14,8 @@ import java.util.regex.Pattern;
 public class Practica4 {
     static String nombreArchivo = "./u4/practica4.txt";
     static List<String> palabrasDeArchivo = new ArrayList<>();
+   static SinglyLinkedList <PalabraPosicion> palabrasDelArchivo = new SinglyLinkedList<>();
+
     private static boolean archivoLecturaTieneContenido() {
         try {
             FileReader fr = new FileReader(nombreArchivo);
@@ -36,9 +38,10 @@ public class Practica4 {
             FileReader fr = new FileReader(nombreArchivo);
             BufferedReader br = new BufferedReader(fr);
             String linea;
-
+            int numLinea = 0;
             while ((linea = br.readLine()) != null) {  //leer el archivo por lineas
-               logicaLectura(linea);
+                numLinea++;
+               logicaLectura(linea,numLinea);
             }
             br.close();//cerrar lectura de archivo
             JOptionPane.showMessageDialog(null, "Archivo leido con exito");
@@ -51,7 +54,7 @@ public class Practica4 {
         }
         return false;
     }
-    private static void logicaLectura(String linea){
+    private static void logicaLectura(String linea,int numLinea){
         // Divide la línea en segmentos separados por comas
         String[] segmentos = linea.split(",");
         // Patrón para constantes de tipo String
@@ -64,32 +67,41 @@ public class Practica4 {
             if (mComentario.find()) {
                 // Si el segmento es un comentario, trata todo el segmento como un comentario
                 palabrasDeArchivo.add(mComentario.group());
+                palabrasDelArchivo.addLast(new PalabraPosicion(mComentario.group(),numLinea));
             } else if (mString.find()) {
                 // Si el segmento es una constante de tipo String, trata todo el segmento como una constante de tipo String
                 palabrasDeArchivo.add(mString.group());
+                palabrasDelArchivo.addLast(new PalabraPosicion(mString.group(),numLinea));
             } else {
                 // Si el segmento no tiene un comentario ni una constante de tipo String, divide el segmento en palabras
                 String[] palabras = segmento.split("\\s+");
                 for (String palabra : palabras) {
                     if (!palabra.trim().isEmpty()) { // Verificar si la cadena no está vacía después de eliminar los espacios en blanco
                         palabrasDeArchivo.add(palabra.trim()); //Agregar las palabras al ArrayList después de eliminar los espacios en blanco
+                        palabrasDelArchivo.addLast(new PalabraPosicion(palabra.trim(),numLinea));
                     }
                 }
             }
             palabrasDeArchivo.add(","); // Agrega la coma después de procesar cada segmento
+            palabrasDelArchivo.addLast(new PalabraPosicion(",",numLinea));
         }
         // Elimina la última coma que se agregó al final de la línea
         if (!palabrasDeArchivo.isEmpty() && palabrasDeArchivo.get(palabrasDeArchivo.size() - 1).equals(",")) {
             palabrasDeArchivo.remove(palabrasDeArchivo.size() - 1);
+        }
+        if (!palabrasDelArchivo.isEmpty() && palabrasDelArchivo.last().getPalabra().equals(",")) {
+            palabrasDelArchivo.removeLast();
         }
     }
 
     public static void main(String[] args) {
         if (leerArchivo()) {
             //JOptionPane.showMessageDialog(null, "Palabras en el archivo: " + palabrasDeArchivo);
-            for (String palabra: palabrasDeArchivo) {
-                System.out.println(palabra);
-            }
+            //for (String palabra: palabrasDeArchivo) {
+              //  System.out.println(palabra);
+            //}
+            System.out.println("Palabras en el archivo:\n " + palabrasDelArchivo);
+            //palabrasDelArchivo.toString();
             //System.out.println("\n" + palabrasDeArchivo);
         }
     }
