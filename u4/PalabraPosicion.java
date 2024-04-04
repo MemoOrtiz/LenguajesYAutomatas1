cd1a5bbe07abf9b8d2709533670d6ff06ef73f92
“MemoOrtizgit config --global user.email “memodor2001@gmail.com <memodor2001@gmail.com>
2024-04-04 03:48:25 -0600

    Modificaciones en atributos y metodos get y setters para valor token y esIdentificador

--- u4/PalabraPosicion.java
+++ u4/PalabraPosicion.java
@@ -2,17 +2,19 @@ package u4;
 
 public class PalabraPosicion {
     private String palabra;
+    private int valorToken;
+    private int esIdentificador;
     private int posicion;
 
     public PalabraPosicion(String palabra, int posicion) {
         this.palabra = palabra;
         this.posicion = posicion;
     }
-
     public String getPalabra() {
         return palabra;
     }
-
+    public int getValorToken() { return valorToken; }
+    public int getEsIdentificador() { return esIdentificador; }
     public int getPosicion() {
         return posicion;
     }
@@ -20,7 +22,8 @@ public class PalabraPosicion {
     public void setPalabra(String palabra) {
         this.palabra = palabra;
     }
-
+    public void setValorToken(int valorToken) { this.valorToken = valorToken; }
+    public void setEsIdentificador(int esIdentificador) { this.esIdentificador = esIdentificador; }
     public void setPosicion(int posicion) {
         this.posicion = posicion;
     }
