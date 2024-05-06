1ea24b34f851f1ab4cd181f2a2e16b4bc3e950a6
“MemoOrtizgit config --global user.email “memodor2001@gmail.com <memodor2001@gmail.com>
2024-05-06 00:31:10 -0600

    Se añadio el archivo del analisis Sintactico, hasta este commit solo clasifica hasta lo declarativo

--- u5/Practica5.java
+++ u5/Practica5.java
@@ -9,10 +9,12 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.Arrays;
+import java.util.LinkedList;
 
 public class Practica5 {
     static String nombreArchivo = "./u4/Tabla de Tokens.txt";
-    static SinglyLinkedList<DatosPalabra> listaTokens = new SinglyLinkedList<>();
+    //static SinglyLinkedList<DatosPalabra> listaTokens = new SinglyLinkedList<>();
+    static LinkedList<DatosPalabra> listaDeTokens = new LinkedList<>();
 
     private static boolean archivoLecturaTieneContenido() {
         try {
@@ -64,13 +66,15 @@ public class Practica5 {
             String id = partes[3];
             String last = partes[4]; // Guardar el último número
 
-            listaTokens.addLast(new DatosPalabra(lexema, Integer.parseInt(token), Integer.parseInt(id), Integer.parseInt(last)));
-            // Imprimir los resultados
+            listaDeTokens.add(new DatosPalabra(lexema, Integer.parseInt(token), Integer.parseInt(id), Integer.parseInt(last),numLinea));
+            //listaTokens.addLast(new DatosPalabra(lexema, Integer.parseInt(token), Integer.parseInt(id), Integer.parseInt(last)));
+            /*// Imprimir los resultados
             System.out.println("Lexema: " + lexema);
             System.out.println("Token: " + token);
             System.out.println("ID: " + id);
             System.out.println("Last: " + last);
             System.out.println("Número de línea tabla tokens: " + numLinea);
+        */
         } else {
             // Dividir la línea en partes usando la coma como delimitador
             String[] partes = linea.split(",", -1);
@@ -81,20 +85,137 @@ public class Practica5 {
             String id = partes[2];
             String last = partes[3];
 
-            listaTokens.addLast(new DatosPalabra(lexema, Integer.parseInt(token), Integer.parseInt(id), Integer.parseInt(last)));
+            listaDeTokens.add(new DatosPalabra(lexema, Integer.parseInt(token), Integer.parseInt(id), Integer.parseInt(last),numLinea));
+            //listaTokens.addLast(new DatosPalabra(lexema, Integer.parseInt(token), Integer.parseInt(id), Integer.parseInt(last)));
             // Imprimir los resultados
-            System.out.println("Lexema: " + lexema);
+            /*System.out.println("Lexema: " + lexema);
             System.out.println("Token: " + token);
             System.out.println("ID: " + id);
             System.out.println("Last: " + last);
-            System.out.println("Número de línea tabla tokens: " + numLinea);
+            System.out.println("Número de línea tabla tokens: " + numLinea);*/
         }
     }
 
+    public static int validarInicio(LinkedList<DatosPalabra> listaDeTokens ,int numLinea){
+        while(numLinea < listaDeTokens.size()){
+            if(listaDeTokens.get(numLinea).getValorToken() == -1) {
+                numLinea++;
+            }
+            if (listaDeTokens.get(numLinea).getValorToken() == -55) {
+                numLinea++;
+            }
+            if (listaDeTokens.get(numLinea).getValorToken() == -75) {
+                numLinea++;
+                continue;
+            }
+            numLinea = validarSentencia(listaDeTokens,numLinea);
+        }
+        return numLinea;
+    }
+
+    public static int validarIdentificadores(LinkedList<DatosPalabra> listaDeTokens ,int numLinea){
+        if(listaDeTokens.get(numLinea).getValorToken() == -51) {
+            numLinea++;
+            return numLinea;
+        }else if(listaDeTokens.get(numLinea).getValorToken() == -52){
+            numLinea++;
+            return numLinea;
+        }else if(listaDeTokens.get(numLinea).getValorToken() == -53){
+            numLinea++;
+            return numLinea;
+
+        }else if(listaDeTokens.get(numLinea).getValorToken() == -54){
+            numLinea++;
+            return numLinea;
+        } else if(listaDeTokens.get(numLinea).getValorToken() == -55){
+            numLinea++;
+            return numLinea;
+        }
+        return -1;
+    }
+
+
+    public static int validarTipoDato(LinkedList<DatosPalabra> listaDeTokens ,int numLinea){
+        if(listaDeTokens.get(numLinea).getValorToken() == -11){
+            numLinea++;
+            return numLinea;
+        } else if (listaDeTokens.get(numLinea).getValorToken() == -12) {
+            numLinea++;
+            return numLinea;
+        } else if (listaDeTokens.get(numLinea).getValorToken() == -13) {
+            numLinea++;
+            return numLinea;
+        } else if (listaDeTokens.get(numLinea).getValorToken() == -14) {
+            numLinea++;
+            return numLinea;
+        }
+        return -1;
+    }
+    public static int validarSentencia(LinkedList<DatosPalabra> listaDeTokens ,int numLinea){
+        numLinea = validarSentenciaDeclaracion(listaDeTokens,numLinea);
+        return numLinea;
+    }
+    public static int validarSentenciaDeclaracion(LinkedList<DatosPalabra> listaDeTokens ,int numLinea){
+       if (listaDeTokens.get(numLinea).getValorToken() == -15){
+           numLinea++;
+
+       }
+        numLinea = validarDeclaracion(listaDeTokens,numLinea);
+        return numLinea;
+    }
+    public static int validarDeclaracion(LinkedList<DatosPalabra> listaDeTokens ,int numLinea){
+        //llamar validarTipoDato
+        numLinea = validarTipoDato(listaDeTokens,numLinea);
+        if (listaDeTokens.get(numLinea).getValorToken() == -77){
+            numLinea++;
+        }
+        while(listaDeTokens.get(numLinea).getValorToken() == -76
+        || listaDeTokens.get(numLinea).getValorToken() == -51
+        || listaDeTokens.get(numLinea).getValorToken() == -52
+        || listaDeTokens.get(numLinea).getValorToken() == -53
+        || listaDeTokens.get(numLinea).getValorToken() == -54){
+            //llamar validarIdentificadores
+            numLinea = validarIdentificadores(listaDeTokens,numLinea);
+            if (listaDeTokens.get(numLinea).getValorToken() == -76){
+                numLinea++;
+            }
+        }
+        if (listaDeTokens.get(numLinea).getValorToken() == -75){
+            numLinea++;
+            return numLinea;
+        }
+        return -1;
+    }
+    
+
+
+
+    public static int validarOperadoresAritmeticos(LinkedList<DatosPalabra> listaDeTokens ,int numLinea){
+        if(listaDeTokens.get(numLinea).getValorToken() == -24){
+            numLinea++;
+        }
+        return -1;
+    }
+
+    /*public static int validarOperadoresRelacionales(LinkedList<DatosPalabra> listaDeTokens ,int numLinea){
+
+    }
+    public static int validarConstantes(LinkedList<DatosPalabra> listaDeTokens ,int numLinea){
+
+    }*/
 
     public static void main(String[] args) {
         if(leerArchivo()){
-
+            int numLinea = 0;
+            numLinea = validarInicio(listaDeTokens,numLinea);
+            int numeroFinal = listaDeTokens.getLast().getLineaSintactico();
+            System.out.println("Numero final: " + numeroFinal);
+            System.out.println("Numero de linea: " + numLinea);
+            if(numLinea == -1){
+                JOptionPane.showMessageDialog(null, "Error en la linea: " + numLinea);
+            }else{
+                JOptionPane.showMessageDialog(null, "El archivo se ha validado correctamente");
+            }
         }
     }
 }
